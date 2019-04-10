f33d19990da1b1bb55e88a70a95c9bdde7a1cde5
Nguyet Ha Tran <nguyet.ha.tran23@gmail.com>
2019-04-10 17:50:54 +0200

    createFastestPathFromNodes corrigé

--- src/main/org/insa/graph/Node.java
+++ src/main/org/insa/graph/Node.java
@@ -2,6 +2,7 @@ package org.insa.graph;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -155,6 +156,10 @@ public final class Node implements Comparable<Node> {
     public int compareTo(Node other) {
         return Integer.compare(getId(), other.getId());
     }
+
+	public Iterator<Arc> iterator() {
+		return Collections.unmodifiableList(this.successors).iterator();
+	}
 
 	
 
