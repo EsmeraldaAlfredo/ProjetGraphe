731fbb708bb35bd66c03477fa8ea922782584173
EsmeraldaAlfredo <esmerybaby@gmail.com>
2019-05-14 15:44:58 +0200

    eviter merge

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -14,6 +14,7 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
+    	
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         boolean fin = false;
